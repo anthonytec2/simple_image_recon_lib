e5e3798098ecf27077efc8a991d8fdf262914f09
Anthony Bisulco <bisulco.a@husky.neu.edu>
2025-09-29 22:36:52 -0400

    Add in batch processing method

--- python/pybind11_bindings.cpp
+++ python/pybind11_bindings.cpp
@@ -30,6 +30,51 @@ PYBIND11_MODULE(simple_image_recon, m)
              )doc")
 
     .def(
+      "process_events",
+      [](
+        SimpleImageReconstructor & self, py::array_t<uint32_t> timestamps,
+        py::array_t<uint16_t> x_coords, py::array_t<uint16_t> y_coords,
+        py::array_t<uint8_t> polarities) {
+        auto t_buf = timestamps.request();
+        auto x_buf = x_coords.request();
+        auto y_buf = y_coords.request();
+        auto p_buf = polarities.request();
+
+        if (t_buf.ndim != 1 || x_buf.ndim != 1 || y_buf.ndim != 1 || p_buf.ndim != 1) {
+          throw std::runtime_error("All arrays must be 1-dimensional");
+        }
+
+        size_t n = t_buf.shape[0];
+        if (x_buf.shape[0] != n || y_buf.shape[0] != n || p_buf.shape[0] != n) {
+          throw std::runtime_error("All arrays must have the same length");
+        }
+
+        auto * t_ptr = static_cast<uint32_t *>(t_buf.ptr);
+        auto * x_ptr = static_cast<uint16_t *>(x_buf.ptr);
+        auto * y_ptr = static_cast<uint16_t *>(y_buf.ptr);
+        auto * p_ptr = static_cast<uint8_t *>(p_buf.ptr);
+
+        // Release GIL for performance
+        py::gil_scoped_release release;
+
+        for (size_t i = 0; i < n; ++i) {
+          self.event(t_ptr[i], x_ptr[i], y_ptr[i], p_ptr[i]);
+        }
+      },
+      py::arg("timestamps"), py::arg("x"), py::arg("y"), py::arg("polarity"),
+      R"doc(
+        Process multiple events in batch (much faster than calling event() in a loop).
+        
+        Args:
+            timestamps: numpy array of timestamps (uint32)
+            x: numpy array of x-coordinates (uint16)
+            y: numpy array of y-coordinates (uint16)
+            polarity: numpy array of polarities (uint8, 0 or 1)
+            
+        All arrays must have the same length.
+        )doc")
+
+    .def(
       "initialize", &SimpleImageReconstructor::initialize, py::arg("width"), py::arg("height"),
       py::arg("cutoff_time"), py::arg("tile_size"), py::arg("fill_ratio"),
       R"doc(
@@ -74,6 +119,9 @@ PYBIND11_MODULE(simple_image_recon, m)
     .def_property_readonly(
       "event_window_size", &SimpleImageReconstructor::getEventWindowSize,
       "Current event window size");
+
+  // Note: GAUSSIAN_3x3 and GAUSSIAN_5x5 are static constexpr and used internally
+  // They don't need to be exposed to Python
 
   m.attr("__version__") = "1.0.0";
 }
